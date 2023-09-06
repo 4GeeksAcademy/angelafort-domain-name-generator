37c3d2148f6c7096e31116f2106319842ea60a61
angelafort <139291628+angelafort@users.noreply.github.com>
2023-09-06 23:34:39 +0000

    domain name generator done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+//window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let red = [".com", ".net", ".com.ve"];
+
+  for (let pro of pronoun) {
+    for (let ad of adj) {
+      for (let sust of noun) {
+        for (let dns of red)
+        console.log(pro+ad+sust+dns);
+      }
+    }
+  }
+  
+
